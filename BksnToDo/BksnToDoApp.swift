fccd2e5c9294c9a62f80fc8c4496a62a1db8fa66
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2023-03-15 03:03:54 +0900

    #1 プロジェクト作成

---
+++ BksnToDo/BksnToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BksnToDoApp.swift
+//  BksnToDo
+//
+//  Created by Suguru Takahashi on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct BksnToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
